321e606c81d09e50e8b5d1e60e80d568f1b39890
carlobortolan <carlobortolan@gmail.com>
2023-06-08 17:05:29 +0200

    Updated engine

--- engine.py
+++ engine.py
@@ -17,6 +17,7 @@ class Value:
         return f"Value(data = {self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -27,6 +28,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -35,6 +37,27 @@ class Value:
 
         out._backward = _backward
         return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers"
+        out = Value(self.data ** other, (self,), f'**(other)')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+
+        out._backward = _backward
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other ** -1
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def __tanh__(self):
         x = self.data
@@ -45,6 +68,15 @@ class Value:
             self.grad += (1 - t ** 2) * out.grad
 
         return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
 
     def backward(self):
         topo = []
