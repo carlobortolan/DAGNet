6400017f6a7bbdfc3bc5265538fb53c3e153aa4d
carlobortolan <carlobortolan@gmail.com>
2023-06-08 16:16:20 +0200

    Added topological sort

--- engine.py
+++ engine.py
@@ -20,8 +20,8 @@ class Value:
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
         out._backward = _backward
         return out
@@ -30,8 +30,8 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = out.grad * other.data
-            other.grad = out.grad * self.data
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
 
         out._backward = _backward
         return out
@@ -42,6 +42,23 @@ class Value:
         out = Value(t, (self,), 'tanh')
 
         def _backward():
-            self.grad = (1 - t ** 2) * out.grad
+            self.grad += (1 - t ** 2) * out.grad
 
         return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward(self)
