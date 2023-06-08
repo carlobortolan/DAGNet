93c9bc34803bc6509c2ae42c9cf02e42aa9554e7
carlobortolan <carlobortolan@gmail.com>
2023-06-08 15:10:02 +0200

    Added tanh

--- engine.py
+++ engine.py
@@ -1,11 +1,15 @@
+import math
+
+
 class Value:
     """ stores a single scalar value and its gradient """
 
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
-        self.grad = 0
+        self.grad = 0.0
         # internal variables used for autograd graph construction
-        self.prev = set(_children)
+        self._backward = lambda: None
+        self._prev = set(_children)
         self._op = _op  # the op that produced this node, for graphviz / debugging / etc
         self.label = label
 
@@ -14,8 +18,30 @@ class Value:
 
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+
+        out._backward = _backward
         return out
 
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad = out.grad * other.data
+            other.grad = out.grad * self.data
+
+        out._backward = _backward
+        return out
+
+    def __tanh__(self):
+        x = self.data
+        t = (math.exp(2 + x) - 1) / (math.exp(2 + x) + 1)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t ** 2) * out.grad
+
         return out
